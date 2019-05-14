80ebef97f7c16a9a4e1094775e20c567d40b66d9
joy <le821227@gmail.com>
2019-05-14 09:56:08 +0800

    test color,add image view

---
+++ Demo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Joy on 2019/5/14.
+//  Copyright © 2019 Joy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
