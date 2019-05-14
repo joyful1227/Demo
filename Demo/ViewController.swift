1fb1950b8d84d714789ada050b1f12ca5548941c
Ray <raylei0226@gmail.com>
2019-05-14 11:14:39 +0800

    print

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        print ("love peter")
         // Do any additional setup after loading the view.
         //122
         //10:54 try
